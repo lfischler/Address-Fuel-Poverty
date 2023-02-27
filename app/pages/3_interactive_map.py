9b0cabe7e8d01d0a1bbbae615d70b2da9bf19597
Leanne Fischler <330269@tutanota.com>
2023-02-27 22:48:37 +0000

    Comment interactive map for legibility

--- app/pages/3_interactive_map.py
+++ app/pages/3_interactive_map.py
@@ -1,45 +1,63 @@
-# %%
+# import libraries
 import pandas as pd
 import streamlit as st
 from streamlit_folium import st_folium
 import folium
-# from folium.plugins import StripePattern
 import os
 import branca
-# import numpy as np
 import json
-# import time
 import ast
 
-colorscale = branca.colormap.LinearColormap(['#800080',
-                                             '#FF0000',
-                                             '#FFA500',
-                                             '#FFFF00',
-                                             '#00FF00']
-                                            ).scale(1, 100)
-# BASE_DIR = os.path.join(os.path.dirname(__file__), '..', '..')
+# set global variables
 app_dir = os.path.dirname(os.path.dirname(__file__))
+# this variable points to the main folder of the app
 
 
-@st.cache_data
-def load_data():
+@st.cache_data  # cache the data that was loaded for quicker loadtimes
+def load_postcode_data():
+    '''
+    Loads the data required for running the interactive fuel poverty map.
+    This data was created in the correct format using the data-restructuring.py
+    file.
+
+    Outputs: List of series objects containing:
+        Scottish Index of Multiple Deprivation Score per postcode,
+        Mean EPC score per postcode,
+        Generated fuel poverty risk rating per postcode.
+    '''
 
     st.spinner(text="Loading Data...")
+    # show data is loading
 
+    # load data in format created using data-restructuring.py
     fuel_poverty_df_filepath = os.path.join(app_dir,
                                             'data',
                                             'fuel_poverty_data.csv')
     fuel_poverty_df = pd.read_csv(fuel_poverty_df_filepath,
                                   index_col='Postcode')
 
+    # create series' from main dataframe for building map layers and store in a
+    # list for iterating through. Rounds values and saves in correct format for
+    # reading by folium map (using 'None' values)
     simd_series = fuel_poverty_df['simd_percentile']
     simd_series = round(simd_series).fillna(-1).astype(int).replace(-1, None)
-
     epc_series = fuel_poverty_df['average_energy_efficiency_rating']
     epc_series = round(epc_series).fillna(-1).astype(int).replace(-1, None)
-
     fpr_series = fuel_poverty_df['fuel_poverty_rating']
     fpr_series = round(fpr_series).fillna(-1).astype(int).replace(-1, None)
+    series_list = [simd_series, epc_series, fpr_series]
+
+    return series_list
+
+
+@st.cache_data
+def load_council_area_df():
+    '''
+    import the council area dataframe with summary data for each area
+    including bounds data.
+
+    Output: area summary dataframe including area boundary details.
+    '''
 
     council_areas_df_filepath = os.path.join(app_dir,
                                              'data',
@@ -47,76 +65,18 @@ def load_data():
     council_areas_df = pd.read_csv(council_areas_df_filepath,
                                    index_col='District')
     council_areas_df.columns = ['postcodes', 'bounds']
+    # load and import council areas dataframe created in data-restructuring.py
 
-    # simd_full_df = pd.read_csv(os.path.join(
-    #     BASE_DIR, 'data', 'postcode_simd.csv'))
-    # simd_rank_series = simd_full_df.set_index('Postcode')['SIMD2020v2_Rank']
-    # simd_series = (simd_rank_series / simd_rank_series.max())*100
-    # simd_series = round(simd_series).astype(int)
-
-    # epc_mean_df = pd.read_csv(os.path.join(BASE_DIR,
-    #                                        'data',
-    #                                        'epc_grouped_2012-22.csv'))
-    # epc_series = epc_mean_df.set_index('Postcode')[
-    #     'average_energy_efficiency_rating']
-    # epc_series = round(epc_series).astype(int)
-
-    # fpr_series = simd_series.add(epc_series)/2
-    # fpr_series = round(fpr_series).fillna(-1).astype(int).replace(-1, None)
-
-    # council_areas = pd.read_csv('https://www.opendata.nhs.scot/dataset/'
-    #                             '9f942fdb-e59e-44f5-b534-d6e17229cc7b/'
-    #                             'resource/'
-    #                             '967937c4-8d67-4f39-974f-fd58c4acfda5/'
-    #                             'download/'
-    #                             'ca11_ca19.csv')
-    # council_area_code_df = council_areas.set_index('CAName')['CA']
-    # council_area_code_df = council_area_code_df.drop_duplicates()
-
-    # council_area_names = council_area_code_df.index.unique()
-    # council_area_names_list = council_area_names.sort_values().to_list()
-    # simd_series = full_df['simd_percentile']
-    # epc_series = full_df['average_energy_efficiency_rating']
-
-    # postcode_lookup_df = pd.read_csv(os.path.join(BASE_DIR,
-    #                                               'data',
-    #                                               'scottish_postcodes_lookup'
-    #                                               '.csv'),
-    #                                  usecols=['RegistrationDistrict2007Code',
-    #                                           'PostcodeDistrict'])
-    # council_area_postcodes = postcode_lookup_df \
-    #     .groupby('RegistrationDistrict2007Code')['PostcodeDistrict'] \
-    #     .apply(lambda x: list(np.unique(x)))
-
-    return simd_series, epc_series, fpr_series, council_areas_df
+    return council_areas_df
 
 
 @st.cache_data
-# def import_and_merge_geojson(list_of_postcodes):
 def import_geojson(district):
+    '''
+    Import shape files for each local area.
 
-    # for i in range(len(list_of_postcodes)):
-    #     filepath = os.path.join(BASE_DIR,
-    #                             'data',
-    #                             'geo_data',
-    #                             f'{list_of_postcodes[i]}.geojson')
-    #     if not os.path.exists(filepath):
-    #         f = open(os.path.join(BASE_DIR,
-    #                               'data',
-    #                               'geo_data',
-    #                               '01_blank.geojson'))
-    #     else:
-    #         f = open(filepath)
-
-    #     data = json.load(f)
-    #     data_list.append(data)
-
-    # for i in range(len(data_list)):
-    #     if i == 0:
-    #         bigger_dict = data_list[i]
-
-    #     else:
-    #         bigger_dict['features'].extend(data_list[i]['features'])
+    Output: dictionary containing featurecollection.
+    '''
 
     filepath = os.path.join(app_dir,
                             'data',
@@ -127,25 +87,38 @@ def import_geojson(district):
                               'data',
                               'geo_data_by_district',
                               '01_blank.geojson'))
+    # if no file exists, import blank geojson
     else:
         f = open(filepath)
     data = json.load(f)
+    # import data and load using json format to python dictionary
 
     return data
 
 
 def add_layer(geojson_data_dict, series, show, name):
-    # stripe = StripePattern(angle=45,
-    #                        color='grey',
-    #                        space_color='white')
+    '''Add a layer of polygon data to the map.'''
+
+    colorscale = branca.colormap.LinearColormap(['#800080',
+                                                 '#FF0000',
+                                                 '#FFA500',
+                                                 '#FFFF00',
+                                                 '#00FF00']
+                                                ).scale(1, 100)
+    # this colourscale is used to colour the scores on each polygon
 
     def create_style(feature):
+        '''creates a style dictionary for the polygons'''
+
         rank = series.get(feature["properties"]["postcodes"], None)
+        # get a rank value for that polygon's data
+
+        # set rank to colour unless the value is empty
+        # also set border style
         if rank is None:
             style_dict = {
                 "fillOpacity": 0,
                 "weight": 0.4,
-                # 'fillPattern': sp}
                 'fillColor': 'white'}
         else:
             style_dict = {
@@ -155,61 +128,50 @@ def add_layer(geojson_data_dict, series, show, name):
         return style_dict
 
     def amend_highlight(feature):
+        '''creates style dictionary for highlighted polygon'''
+
         rank = series.get(feature["properties"]["postcodes"], None)
+        # get a rank value for that polygon's data
+
         highlight_dict = {'fillOpacity': 0 if rank is None else 0.7,
                           'weight': 2,
                           'fillColor': "#white" if rank is None
                           else colorscale(rank)}
+        # sets style for highlights including lineweight, colour, opacity
+
         return highlight_dict
 
-    # def create_tooltip(feature):
-    #     rank = series.get(feature["properties"]["postcodes"])
-    #     return folium.features.GeoJsonTooltip(
-    #         fields=['postcodes',
-    #                 rank],
-    #         aliases=["Postcode: ",
-    #                  "Score: "])
-
-    # def get_rank(postcode):
-    #     return series.get(postcode)
-
+    # build geojson map with tooltip, name and styles
     return folium.GeoJson(geojson_data_dict,
                           style_function=create_style,
                           highlight_function=amend_highlight,
                           show=show,
                           name=name,
                           overlay=False,
-                          #   tooltip=create_tooltip(feature))
                           tooltip=folium.features.GeoJsonTooltip(
-                            fields=['postcodes'],
+                            fields=['postcodes'],  # show postcode when
+                                                   # highlighted
                             aliases=["Postcode:  "]))
 
 
 @st.cache_resource()
-def load_map(district, simd_series, epc_series, fpr_series, areas_df):
+def load_map(district, series_list, areas_df):
 
     geojson_data_dict = import_geojson(district)
 
     map = folium.Map(tiles=None, prefer_canvas=True)
     folium.TileLayer('cartodbpositron', control=False).add_to(map)
 
-    # sp = StripePattern(angle=45, color='grey', space_color='white')
-    # sp.add_to(map)
+    layer_display_status = [True, False, False]
+    layer_names = ['Fuel Poverty Risk Rating',
+                   'Index of Multiple Deprivation',
+                   'EPC Mean for Postcode']
 
-    add_layer(geojson_data_dict,
-              fpr_series,
-              True,
-              'Fuel Poverty Risk Rating').add_to(map)
-
-    add_layer(geojson_data_dict,
-              simd_series,
-              False,
-              'Index of Multiple Deprivation').add_to(map)
-
-    add_layer(geojson_data_dict,
-              epc_series,
-              False,
-              'EPC Mean for Postcode').add_to(map)
+    for i in range(len(series_list)):
+        add_layer(geojson_data_dict,
+                  series_list[i],
+                  layer_display_status[i],
+                  layer_names[i]).add_to(map)
 
     # map.fit_bounds(map.get_bounds(), padding=(-2, -2))
     bounds = ast.literal_eval(areas_df.loc[district]['bounds'])
@@ -222,7 +184,9 @@ def load_map(district, simd_series, epc_series, fpr_series, areas_df):
 def main():
 
     # %%
-    simd_series, epc_series, fpr_series, council_areas_df = load_data()
+    # simd_series, epc_series, fpr_series, council_areas_df = load_data()
+    series_list = load_postcode_data()
+    council_areas_df = load_council_area_df()
 
     # st.set_page_config(layout='wide')
     st.title('Interactive Fuel Poverty Map')
@@ -235,25 +199,14 @@ def main():
 
     # %%
 
-    # requested_council_area = 'Aberdeen City'
     if requested_council_area != ' ':
-        # progress_text = "Operation in progress. Please wait."
-        # my_bar = st.progress(0, text=progress_text)
 
-        # for percent_complete in range(100):
-        #     time.sleep(0.05)
-        #     my_bar.progress(percent_complete + 1, text=progress_text)
-
-        # selected_council_area_postcodes = get_postcodes(
-        #     requested_council_area,
-        #     council_area_postcodes,
-        #     council_area_code_df)
         selected_council_area_postcodes = council_areas_df.loc[
             requested_council_area]['postcodes']
 
         map = load_map(requested_council_area,
-                       simd_series, epc_series,
-                       fpr_series, council_areas_df)
+                       series_list,
+                       council_areas_df)
         st.write('Building map...'
                  'There are a lot of polygons to load—so please be patient.')
         st_map = st_folium(map, width=700, height=450)
